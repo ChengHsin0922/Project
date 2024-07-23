ceb606affb48de1ebad850fb32e90fc795506019
ChengHsin0922 <yoyo12825@gmail.com>
2024-07-23 16:20:26 +0800

    202407231620

--- 0717project/app.js
+++ 0717project/app.js
@@ -7,11 +7,28 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", function (req, res) {
     res.send('ok');
 });
-//市集地圖
-app.get("/map", function (req, res) {
-    res.render('map.ejs', {});
+// //市集地圖
+app.get('/map', function(req, res) {
+    conn.query(
+        "SELECT * FROM vendor_info WHERE vinfo = ?",
+        [vinfo],
+        function(err, result) {
+            console.log(result);
+            res.render('map.ejs',{info: result});
+        }
+    )
 })
 //我要擺攤
 app.get("/rentVendor", function (req, res) {
     res.render('rentVendor.ejs', {});
 })
+
+
+var mysql = require("mysql");
+var conn = mysql.createConnection({
+    user:'root',
+    password:'',
+    host:'localhost',
+    database:'haoshin'
+})
+
